f6308d498ffd954c9ba96dfd1cac57a4fd87b3d5
kai <k.peters@mikrocentrum.nl>
2022-11-04 16:27:08 +0100

    initial

---
+++ src/app/DTO/login-user.ts
@@ -0,0 +1,6 @@
+export class LoginUser {
+    public username: string | undefined;
+    public password: string | undefined;
+
+    constructor() {}
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './Content/login/login.component';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+import { LoginUser } from './DTO/login-user';
+import { Router } from '@angular/router';
+import { Token } from './DTO/token';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
